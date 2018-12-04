cf4357c8c7db485c8967cfd9138163efc5a05e25
Ife Odugbesan <ifeodugbesan@yahoo.com>
2018-12-04 19:57:30 +0000

    first react application

--- src/App.js
+++ src/App.js
@@ -1,28 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import dc from './dc_logo.png';
+import cap from './captain_america.png';
 import './App.css';
+
+function Home() {
+  return (
+    <div>
+
+        <h1>Welcome to our first React App!</h1>
+        <div className="container">
+          <div className="column">
+            <img src={dc} className="app-dc" alt="stark"/>
+          </div>
+          <div className="column">
+            <img src={cap} className="app-cap" alt="lannister"/>
+          </div>
+        </div>
+
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Home/>
+      </div>      
     );
   }
 }
 
 export default App;
+
+{/* <header className="App-header">
+<img src={logo} className="App-logo" alt="logo" />
+<p>
+  Edit <code>src/App.js</code> and save to reload.
+</p>
+<a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Learn React
+</a>
+</header> */}
